47d4d7b76d613e229488b5c0b576588795e75c7f
BarcaBro1022 <you.k2208@gmail.com>
2019-09-09 19:17:04 -0700

    commented out render fighters function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,56 @@
-// hp, ap, cp
-let playerHealth = 100
-let cpuHealth = 100
-let attack = ``
-let counter = ``
+// fighter stats
+let fighter1 = {
+    name: `Cloud`,
+    hp: 175,
+    ap: 5,
+    cp: 10
+  }
+  let fighter2 = {
+    name: `Aeris`,
+    hp: 200,
+    ap: 4,
+    cp: 5
+  }
+  let fighter3 = {
+    name: `Sephiroth`,
+    hp: 185,
+    ap: 6,
+    cp: 30
+  }
+  let fighter4 = {
+    name: `Lightning`,
+    hp: 155,
+    ap: 7,
+    cp: 20
+  }
+
+
+
 
 // render fighters
-const renderFighters = () => {
-  document.getElementById(`fighters`).innerHTML = ``
-  for (let i = 0; i < 4; i++) {
-    const random = Math.floor(Math.random() * 10) + 1
-    let fighterElem = document.createElement(`div`)
-    fighterElem.className = `col s3`
-    fighterElem.innerHTML = `
-    <div class="card">
-      <div class="card-image">
-        <img class="fighter" src="./assets/images/fighter${i + 1}.jpg" data-value="${random}">
-      </div>
-    </div>
-    `
-    document.getElementById(`fighters`).append(fighterElem)
+// const renderFighters = () => {
+//   
+//   document.getElementById(`fighters`).innerHTML = ``
+//   for (let i = 0; i < 4; i++) {
+//     let fighterElem = document.createElement(`div`)
+//     fighterElem.className = `col s3`
+//     fighterElem.innerHTML = `
+//     <div class="card">
+//       <div class="card-image">
+//         <img class="fighter" id="fighter${i + 1}" src="./assets/images/fighter${i + 1}.jpg">
+//         <span class="card-title" id="name${i + 1}"></span>
+//       </div>
+//     </div>
+//     `
+//     document.getElementById(`fighters`).append(fighterElem)
+//     document.getElementById(`name1`).textContent = fighter1.name
+    // document.getElementById(`name2`).textContent = fighter2.name
+    // document.getElementById(`name3`).textContent = fighter3.name
+    // document.getElementById(`name4`).textContent = fighter4.name
+    
   }
-}
 
-renderFighters()
+}
+// renderFighters()
+
+
