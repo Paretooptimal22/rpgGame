478b3b275e4869c0e450a4026e1300c89d114bdb
Paretooptimal22 <you.k2208@gmail.com>
2019-09-15 00:41:11 -0700

    added clickCount var and condition

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -30,108 +30,117 @@ document.getElementById(`hp3`).innerHTML = `HP: ` + fighter3.hp
 document.getElementById(`hp4`).innerHTML = `HP: ` + fighter4.hp
 
 // select player event
-document.addEventListener(`click`, event => {
-  // fighters
-  let fighter1 = {
-    name: `Cloud`,
-    hp: 175,
-    ap: 5,
-    cp: 10
-  }
-  let fighter2 = {
-    name: `Aeris`,
-    hp: 200,
-    ap: 4,
-    cp: 5
-  }
-  let fighter3 = {
-    name: `Sephiroth`,
-    hp: 185,
-    ap: 6,
-    cp: 30
-  }
-  let fighter4 = {
-    name: `Lightning`,
-    hp: 155,
-    ap: 7,
-    cp: 20
-  }
-  // Cloud's element
-  let cloudElem = document.createElement(`div`)
-  cloudElem.className = `col s6`
-  cloudElem.innerHTML = `
-  <div class="card" id="cloud">
-    <div class="card-image">
-      <img class="fighter" src="./assets/images/fighter1.jpg">
-      <span class="card-title" id="name1">Cloud</span>
-    </div>
-    <div class="card-content">
-      <p id="hp1">HP: ${fighter1.hp}</p>
-    </div>
-  </div>
-  `
-  // Aeris's element
-  let aerisElem = document.createElement(`div`)
-  aerisElem.className = `col s6`
-  aerisElem.innerHTML = `
-  <div class="card" id="aeris">
-    <div class="card-image">
-      <img class="fighter" src="./assets/images/fighter2.jpg">
-      <span class="card-title" id="name2">Aeris</span>
-    </div>
-    <div class="card-content">
-      <p id="hp2">HP: ${fighter2.hp}</p>
-    </div>
-  </div>
-  `
-  // Sephiroth's element
-  let sephElem = document.createElement(`div`)
-  sephElem.className = `col s6`
-  sephElem.innerHTML = `
-  <div class="card" id="sephiroth">
-    <div class="card-image">
-      <img class="fighter" src="./assets/images/fighter3.jpg">
-      <span class="card-title" id="name3">Sephiroth</span>
-    </div>
-    <div class="card-content">
-      <p id="hp3">HP: ${fighter3.hp}</p>
-    </div>
-  </div>
-  `
-  // Lightning's element
-  let lightElem = document.createElement(`div`)
-  lightElem.className = `col s6`
-  lightElem.innerHTML = `
-  <div class="card" id="lightning">
-    <div class="card-image">
-      <img class="fighter" src="./assets/images/fighter4.jpg">
-      <span class="card-title" id="name4">Lightning</span>
-    </div>
-    <div class="card-content">
-      <p id="hp4">HP: ${fighter4.hp}</p>
-    </div>
-  </div>
-  `
+let playerSelect = () => {
+  let clickCount = 0
+  document.addEventListener(`click`, event => {
+    clickCount = clickCount + 1
+    if (clickCount <= 2) {
+      // fighters
+      let fighter1 = {
+        name: `Cloud`,
+        hp: 175,
+        ap: 5,
+        cp: 10
+      }
+      let fighter2 = {
+        name: `Aeris`,
+        hp: 200,
+        ap: 4,
+        cp: 5
+      }
+      let fighter3 = {
+        name: `Sephiroth`,
+        hp: 185,
+        ap: 6,
+        cp: 30
+      }
+      let fighter4 = {
+        name: `Lightning`,
+        hp: 155,
+        ap: 7,
+        cp: 20
+      }
+      // Cloud's element
+      let cloudElem = document.createElement(`div`)
+      cloudElem.className = `col s6`
+      cloudElem.innerHTML = `
+      <div class="card" id="cloud">
+        <div class="card-image">
+          <img class="fighter" src="./assets/images/fighter1.jpg">
+          <span class="card-title" id="name1">Cloud</span>
+        </div>
+        <div class="card-content">
+          <p id="hp1">HP: ${fighter1.hp}</p>
+        </div>
+      </div>
+      `
+      // Aeris's element
+      let aerisElem = document.createElement(`div`)
+      aerisElem.className = `col s6`
+      aerisElem.innerHTML = `
+      <div class="card" id="aeris">
+        <div class="card-image">
+          <img class="fighter" src="./assets/images/fighter2.jpg">
+          <span class="card-title" id="name2">Aeris</span>
+        </div>
+        <div class="card-content">
+          <p id="hp2">HP: ${fighter2.hp}</p>
+        </div>
+      </div>
+      `
+      // Sephiroth's element
+      let sephElem = document.createElement(`div`)
+      sephElem.className = `col s6`
+      sephElem.innerHTML = `
+      <div class="card" id="sephiroth">
+        <div class="card-image">
+          <img class="fighter" src="./assets/images/fighter3.jpg">
+          <span class="card-title" id="name3">Sephiroth</span>
+        </div>
+        <div class="card-content">
+          <p id="hp3">HP: ${fighter3.hp}</p>
+        </div>
+      </div>
+      `
+      // Lightning's element
+      let lightElem = document.createElement(`div`)
+      lightElem.className = `col s6`
+      lightElem.innerHTML = `
+      <div class="card" id="lightning">
+        <div class="card-image">
+          <img class="fighter" src="./assets/images/fighter4.jpg">
+          <span class="card-title" id="name4">Lightning</span>
+        </div>
+        <div class="card-content">
+          <p id="hp4">HP: ${fighter4.hp}</p>
+        </div>
+      </div>
+      `
 
-  // click event to move fighters to battle arena
-  if (event.target.id === `cloud`) {
-    document.getElementById(`arena`).append(cloudElem)
-    document.getElementById(`p1`).innerHTML = ``
-    document.getElementById(`select`).innerHTML = `Select Your Opponent!`
-  } else if (event.target.id === 'aeris') {
-    document.getElementById(`arena`).append(aerisElem)
-    document.getElementById(`p2`).innerHTML = ``
-    document.getElementById(`select`).innerHTML = `Select Your Opponent!`
-  } else if (event.target.id === 'sephiroth') {
-    document.getElementById(`arena`).append(sephElem)
-    document.getElementById(`p3`).innerHTML = ``
-    document.getElementById(`select`).innerHTML = `Select Your Opponent!`
-  } else if (event.target.id === 'lightning') {
-    document.getElementById(`arena`).append(lightElem)
-    document.getElementById(`p4`).innerHTML = ``
-    document.getElementById(`select`).innerHTML = `Select Your Opponent!`
-  } 
-})
+      // click event to move fighters to battle arena
+      if (event.target.id === `cloud`) {
+        document.getElementById(`arena`).append(cloudElem)
+        document.getElementById(`p1`).innerHTML = ``
+        document.getElementById(`select`).innerHTML = `Select Your Opponent!`
+      } else if (event.target.id === 'aeris') {
+        document.getElementById(`arena`).append(aerisElem)
+        document.getElementById(`p2`).innerHTML = ``
+        document.getElementById(`select`).innerHTML = `Select Your Opponent!`
+      } else if (event.target.id === 'sephiroth') {
+        document.getElementById(`arena`).append(sephElem)
+        document.getElementById(`p3`).innerHTML = ``
+        document.getElementById(`select`).innerHTML = `Select Your Opponent!`
+      } else if (event.target.id === 'lightning') {
+        document.getElementById(`arena`).append(lightElem)
+        document.getElementById(`p4`).innerHTML = ``
+        document.getElementById(`select`).innerHTML = `Select Your Opponent!`
+      }
+    }
+  })}
+playerSelect()
+
+
+
 
 
 let attackBtn = document.createElement(`a`)
